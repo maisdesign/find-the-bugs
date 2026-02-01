f84738b42d22d829e9967c2d539c9a46fb41f87b
Maisdesign <downloadtaky@gmail.com>
2026-02-01 18:16:47 +0100

    Implement form submission handling with validation and feedback messages

--- snack-recap-2/script.js
+++ snack-recap-2/script.js
@@ -4,5 +4,11 @@ const messaggio = document.getElementById('messaggio');
 form.addEventListener('submit', function (event) {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
-
+    event.preventDefault();
+    if ((username.trim() === "") || (email.trim() === "")) {
+        messaggio.textContent = "Inserisci username ed email per favore";
+    } else {
+        messaggio.textContent = `Il tuo username é: ${username} e la tua mail invece è: ${email}`;
+    }
 });
+
