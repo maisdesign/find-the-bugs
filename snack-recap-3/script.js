e30a777e585368725bb91c7580eea0479d78e13c
Maisdesign <downloadtaky@gmail.com>
2026-02-01 18:42:05 +0100

    Snack3

---
+++ snack-recap-3/script.js
@@ -0,0 +1,22 @@
+const numeriVincenti = [7, 13, 21, 42, 89];
+const input = document.getElementById('numero-utente');
+const bottone = document.getElementById('verifica');
+const messaggio = document.getElementById('messaggio');
+let indovinato = false;
+
+bottone.addEventListener('click', function () {
+    // IMPORTANTE: NON usare metodi come indexOf(), includes() o find()
+    indovinato = false;
+    numeriVincenti.forEach(
+        (numero) => {
+            if (input.value == numero) {
+                indovinato = true;
+            }
+            if (indovinato === true) {
+                messaggio.textContent = "Hai indovinato";
+            } else {
+                messaggio.textContent = "Non hai indovinato";
+            }
+        }
+    );
+});
