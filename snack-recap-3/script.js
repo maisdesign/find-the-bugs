c6f9f554af09de69995cb3e6d3ab4ce2073cf0db
Maisdesign <downloadtaky@gmail.com>
2026-02-01 19:08:37 +0100

    Aggiunto listener per caricamento completo DOM

--- snack-recap-3/script.js
+++ snack-recap-3/script.js
@@ -1,24 +1,27 @@
-const numeriVincenti = [7, 13, 21, 42, 89];
-const input = document.getElementById('numero-utente');
-const bottone = document.getElementById('verifica');
-const messaggio = document.getElementById('messaggio');
+addEventListener("DOMContentLoaded", (event) => {
+    const numeriVincenti = [7, 13, 21, 42, 89];
+    const input = document.getElementById('numero-utente');
+    const bottone = document.getElementById('verifica');
+    const messaggio = document.getElementById('messaggio');
 
-bottone.addEventListener('click', function () {
-    // IMPORTANTE: NON usare metodi come indexOf(), includes() o find()
-    let indovinato = false;
-    messaggio.className = "";
-    numeriVincenti.forEach(
-        (numero) => {
-            if (Number(input.value) === numero) {
-                indovinato = true;
-            }
-        });
-    if (indovinato === true) {
-        messaggio.textContent = "Hai indovinato";
-        messaggio.classList.add("btn", "btn-success");
-    } else {
-        messaggio.textContent = "Non hai indovinato";
-        messaggio.classList.add("btn", "btn-danger");
-    }
+    bottone.addEventListener('click', function () {
+        // IMPORTANTE: NON usare metodi come indexOf(), includes() o find()
+        let indovinato = false;
+        messaggio.className = "";
+        numeriVincenti.forEach(
+            (numero) => {
+                if (Number(input.value) === numero) {
+                    indovinato = true;
+                }
+            });
+        if (indovinato === true) {
+            messaggio.textContent = "Hai indovinato";
+            messaggio.classList.add("btn", "btn-success");
+        } else {
+            messaggio.textContent = "Non hai indovinato";
+            messaggio.classList.add("btn", "btn-danger");
+        }
 
-});
+    });
+}
+);
